506ddcfa1fbe37f970434e5ba9848711d60819de
Edison Liem <edisonliem417@gmail.com>
2026-02-03 17:58:26 -0500

    Submit T5

--- T5/index.js
+++ T5/index.js
@@ -26,9 +26,88 @@ app.get("/", (req, res) => {
 });
 
 // ADD YOUR WORK HERE
+const data = [
+  {
+    title: "Buy groceries",
+    description: "Milk, Bread, Eggs, Butter",
+    completed: false
+  },
+  {
+    title: "Walk the dog",
+    description: "Take Bella for a walk in the park",
+    completed: true
+  },
+  {
+    title: "Read a book",
+    description: "Finish reading 'The Great Gatsby'",
+    completed: false
+  }
+];
 
+app.get("/notes", (req, res) => {
+    const { done } = req.query;
 
+    if (done !== undefined && done !== "true" && done !== "false") {
+        return res.status(400).send("Bad request");
+    }
 
+    let result = data;
+
+    if (done === "true") {
+        result = data.filter(note => note.completed === true);
+    } else if (done === "false") {
+        result = data.filter(note => note.completed === false);
+    }
+
+    res.json(result);
+});
+
+app.get("/notes/:noteId", (req, res) => {
+    const noteId = req.params.noteId;
+
+    if (!Number.isInteger(noteId)) {
+        return res.status(400).send("Bad request");
+    }
+
+    if (noteId < 0 || noteId >= data.length) {
+        return res.status(404).send("Not found");
+    }
+
+    res.json(data[noteId]);
+});
+
+app.post("/notes", (req, res) => {
+    const newNote = req.body;
+
+    const noteToReturn = structuredClone(newNote);
+    noteToReturn.id = notes.length;
+
+    notes.push(newNote);
+
+    res.status(201).json(noteToInsert);
+});
+
+app.patch("notes/:noteId", (req, res) => {
+    const { done } = req.query;
+    const noteId = req.params.noteId;
+
+    if (done !== "true" && done !== "false") {
+        return res.status(400).send("Bad request");
+    }
+
+    if (!Number.isInteger(noteId)) {
+        return res.status(400).send("Bad request");
+    }
+
+    if (noteId < 0 || noteId >= data.length) {
+        return res.status(404).send("Not found");
+    }
+
+    const note = data[noteId];
+    note.completed = (done === "true");
+
+    res.status(200).json(note);
+});
 // ==================
 
 const server = app.listen(port, () => {
